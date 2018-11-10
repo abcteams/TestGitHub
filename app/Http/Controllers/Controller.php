930e73c5d39f07d2714912d00316eb203790523e
abcteams <miqdad.mohammad@abc-gcc.com>
2018-11-10 16:08:39 +0400

    another change
    this is change

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -12,4 +12,5 @@ class Controller extends BaseController
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
     //this is iqbal edited file
     //please approve this
+    //another change
 }
